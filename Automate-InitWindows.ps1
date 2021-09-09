8bd7c50d26714322ea0260e930a2a1d5da688936
Gavin Wu <39542567+wuyvzhang@users.noreply.github.com>
2021-09-09 14:34:05 +0800

    Add files via upload

---
+++ Automate-InitWindows.ps1
@@ -0,0 +1,35 @@
+﻿#!/bin/bash
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Microsoft Azure
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+Add-WindowsFeature Web-Server
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) !"
+$connectTestResult = Test-NetConnection -ComputerName demo0214.file.core.windows.net -Port 445
+if ($connectTestResult.TcpTestSucceeded) {
+    # 保存密码，以便驱动器在重启时继续保留
+    cmd.exe /C "cmdkey /add:`"demo0214.file.core.windows.net`" /user:`"localhost\demo0214`" /pass:`"UPJlgQJFUgkYPY6Qs0oD7hf9S5aDE+pA6XUfgVXcshgstumi7Y1C4qq16B2IFhw/aXfsb82+MTZCTCfaV91TMg==`""
+    # 安装驱动器
+    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\demo0214.file.core.windows.net\demo" -Persist
+} else {
+    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+}
