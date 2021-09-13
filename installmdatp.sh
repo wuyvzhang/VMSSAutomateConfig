282509897057c8285fd7f071ba9c46a10536202b
Gavin Wu <39542567+wuyvzhang@users.noreply.github.com>
2021-09-13 18:22:34 +0800

    Update installmdatp.sh

--- installmdatp.sh
+++ installmdatp.sh
@@ -1,5 +1,9 @@
 
 #!/usr/bin/shell
+sudo yum install yum-utils
+sudo yum-config-manager --add-repo=https://packages.microsoft.com/config/rhel/7/prod.repo
+sudo rpm --import http://packages.microsoft.com/keys/microsoft.asc
+yum makecache
 sudo yum install mdatp
 yum repolist
 sudo yum --enablerepo=packages-microsoft-com-prod install mdatp
